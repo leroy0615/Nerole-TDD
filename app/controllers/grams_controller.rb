5e6294e6128eac80e6127a7c2c0704859f5ab389
leroy0615 <leroy0615@gmail.com>
2019-08-17 13:50:09 +0000

    last update

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,12 +2,11 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-      @grams = Gram.all
+   
   end
 
   def new
-    @gram=Gram.new
-    
+    @gram = Gram.new
   end
 
   def create
@@ -56,10 +55,12 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message, :picture)
+    params.require(:gram).permit(:message)
   end
 
 
-  
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
 
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,10 +7,8 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+  	
   }
   config.fog_directory  = ENV["AWS_BUCKET"]    
-  if Rails.env.development?
-  config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
-  config.root = '/home/vagrant/uploads_tmp/tmp'
-end   
+  
 end
